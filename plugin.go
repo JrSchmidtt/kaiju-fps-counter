4ca4502e05c7a7079e875ab2e12fc45ad0b68b23
JrSchmidtt <junior.schmidt.99@gmail.com>
2026-03-17 18:11:04 -0300

    Refactor FPS calculation and update sample interval in FPS Counter plugin

--- plugin.go
+++ plugin.go
@@ -2,15 +2,13 @@ package fpscounter
 
 import (
 	"fmt"
-	"log/slog"
-
 	"kaijuengine.com/editor"
 	"kaijuengine.com/editor/editor_plugin"
 )
 
 const (
 	pluginKey      = "github.com/JrSchmidtt/kaiju-fps-counter/"
-	sampleInterval = 0.5
+	sampleInterval = 1.0
 	titlePrefix    = "Kaiju Editor"
 )
 
@@ -19,27 +17,25 @@ type Plugin struct{}
 func init() { editor.RegisterPlugin(pluginKey, &Plugin{}) }
 
 func (p *Plugin) Launch(ed editor_plugin.EditorInterface) error {
-	slog.Info("launching FPS Counter plugin", "key", pluginKey)
-	slog.Warn("FPS Counter plugin loaded")
 	host := ed.Host()
-	host.Window.SetTitle(titlePrefix + " | FPS plugin loaded")
 
-	elapsed := 0.0
-	frames := 0
+	host.RunAfterFrames(5, func() {
+		elapsed := 0.0
 
-	host.Updater.AddUpdate(func(delta float64) {
-		elapsed += delta
-		frames++
+		host.Updater.AddUpdate(func(delta float64) {
+			elapsed += delta
 
-		if elapsed < sampleInterval {
-			return
-		}
+			if elapsed < sampleInterval {
+				return
+			}
 
-		fps := float64(frames) / elapsed
-		host.Window.SetTitle(fmt.Sprintf("%s | FPS: %.1f", titlePrefix, fps))
+			fps := 1.0 / delta
+			host.RunOnMainThread(func() {
+				host.Window.SetTitle(fmt.Sprintf("%s | FPS: %.1f", titlePrefix, fps))
+			})
 
-		elapsed = 0
-		frames = 0
+			elapsed = 0
+		})
 	})
 
 	return nil
